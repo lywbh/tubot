60342fe944fca83977981b67baccfa95d460d388
lgs <1961621226@qq.com>
2021-08-05 18:04:21 +0800

    完成查询功能

---
+++ src/main/java/com/kamimi/tubot/config/KeyConfig.java
@@ -0,0 +1,10 @@
+package com.kamimi.tubot.config;
+
+/**
+ * 各类秘钥
+ */
+public class KeyConfig {
+
+    public static final String SAUCENAO_API_KEY = "";
+
+}
--- src/main/java/com/kamimi/tubot/module/SaucenaoModule.java
+++ src/main/java/com/kamimi/tubot/module/SaucenaoModule.java
@@ -3,6 +3,7 @@ package com.kamimi.tubot.module;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.kamimi.tubot.config.KeyConfig;
 import com.kamimi.tubot.utils.HttpUtils;
 import com.kamimi.tubot.utils.LogUtils;
 
@@ -14,15 +15,13 @@ import java.util.Map;
 public class SaucenaoModule {
 
     private static final String BASE_URL = "https://saucenao.com/search.php";
-    private static final String API_KEY = "";
-
 
     public static Map<String, String> bestMatch(String imgUrl) {
         Map<String, String> result = new HashMap<>();
         if (imgUrl == null || imgUrl.isEmpty()) {
             return result;
         }
-        String url = BASE_URL + "?api_key=" + API_KEY + "&db=999&output_type=2&testmode=1&url=" + URLEncoder.encode(imgUrl, StandardCharsets.UTF_8);
+        String url = BASE_URL + "?api_key=" + KeyConfig.SAUCENAO_API_KEY + "&db=999&output_type=2&testmode=1&url=" + URLEncoder.encode(imgUrl, StandardCharsets.UTF_8);
         LogUtils.getLogger().info("啊啊啊:" + url);
         String response = HttpUtils.get(url);
         JSONArray respJson = JSON.parseObject(response).getJSONArray("results");
